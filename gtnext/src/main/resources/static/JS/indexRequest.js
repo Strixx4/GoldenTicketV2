17fe6aad8de4385f6dce3c3fc57e69fc36829436
Andrea Sbabo <andreasbabo6@gmail.com>
2023-07-15 19:17:15 +0200

    aggiunto un piccolo bootsrap per le card

--- gtnext/src/main/resources/static/JS/indexRequest.js
+++ gtnext/src/main/resources/static/JS/indexRequest.js
@@ -3,24 +3,24 @@ document.addEventListener('DOMContentLoaded', function () {
   popolateByUrl('/api/eventi/limit?page=0&size=8');
 });
 
-function prevDocumentPage(){
-  if(--page < 0){
-      page = 0;
+function prevDocumentPage() {
+  if (--page < 0) {
+    page = 0;
   }
-  url='/api/eventi/limit?page='+page+'&size=8';
+  url = '/api/eventi/limit?page=' + page + '&size=8';
   popolateByUrl(url)
 }
 
-function nextDocumentPage(){
-url='/api/eventi/limit?page='+ (++page) +'&size=8';
-popolateByUrl(url);
+function nextDocumentPage() {
+  url = '/api/eventi/limit?page=' + (++page) + '&size=8';
+  popolateByUrl(url);
 }
 
 function popolateByUrl(url) {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      if(data.content==""){
+      if (data.content == "") {
         prevDocumentPage();
       }
       populateHTML(data);
@@ -31,23 +31,21 @@ function populateHTML(data) {
   var dataContainer = document.getElementById('context');
   var html = '';
   data.content.forEach(item => {
-    html += '<div class="card">';
-    html += '<div class="img-card">';
-    html += '<img src="' + item.locandina + '" alt="Locandina" width="200px"/>';
+    html += '<div class="card mb-3" style="max-width: 540px;">';
+    html += '<div class="row g-0">';
+    html += '<div class="col-md-4" >';
+    html += '<img src="' + item.locandina + '" class="img-fluid rounded-start"  alt="Evento">'
+    html += '</div>';
+    html += '<div class="col-md-8">';
+    html += '<div class="card-body">';
+    html += '<h5 class="card-title">' + item.nome + '</h5>';
+    html += '<p class="card-text">' + item.localita.citta + ' in: ' + item.localita.zona + '</p>';
+    html += '<p class="card-text"><small class="text-body-secondary">' + item.data + '</small></p>';
+    html += '</div>';
+    html += '</div>';
+    html += '</div>';
     html += '</div>';
 
-    html += '<div class="nome-card">';
-    html += '<h1>' + item.nome + '</h1>';
-    html += '</div>';
-
-    html += '<div class="paragrafo-card">';
-    html += '<p>';
-    html += item.id+'<br>';
-    html += item.localita.citta;
-    html += item.localita.zona + '<br>';
-    html += item.data;
-    html += '</p></div>'
-    html += '</div>';
   });
   dataContainer.innerHTML = html;
 }
