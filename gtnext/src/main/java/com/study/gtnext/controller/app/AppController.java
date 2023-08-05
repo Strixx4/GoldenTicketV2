d0e165e669407d8d71cdf6634841b2bbb8193549
Andrea Sbabo <andreasbabo6@gmail.com>
2023-08-05 15:41:14 +0200

    JS navbar

--- gtnext/src/main/java/com/study/gtnext/controller/app/AppController.java
+++ gtnext/src/main/java/com/study/gtnext/controller/app/AppController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class AppController {
 
-    @GetMapping("login")
-    public String app(){
-        return "login.html";
+    @GetMapping("")
+    public String home(){
+        return "redirect:index";
     }
 
     @GetMapping("index")
---
+++ gtnext/src/main/resources/static/JS/Nav.js
@@ -0,0 +1,41 @@
+function NavBar() {
+    var html=`<nav class="navbar navbar-expand-lg bg-black navbar-dark text-white">
+		<div class="container-fluid">
+			<span class="navbar-brand" >GTNext</span>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+				data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="index">Home</a>
+					</li>
+					<li class="nav-item dropdown">
+						<span class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
+							aria-expanded="false">
+							Località
+					</span>
+						<ul class="dropdown-menu" id="listCitta">
+							<!--<li><a class="dropdown-item" href="#">Action</a></li>-->
+						</ul>
+					</li>
+					<li class="nav-item dropdown">
+						<span class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
+							aria-expanded="false">
+							Tipologia
+					</span>
+						<ul class="dropdown-menu" id="listTipologia">
+							<!--<li><a class="dropdown-item" href="#">Action</a></li>-->
+						</ul>
+					</li>
+				</ul>
+				
+					<button class="btn btn-outline-success broTTone text-white">Log</button>
+				
+			</div>
+		</div>
+	</nav>`;
+    document.write(html)
+}
